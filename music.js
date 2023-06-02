e6ba494a5e75b23a715f803f7d5224cc70bad115
Lee-s-an <dltks2772@naver.com>
2023-06-02 14:51:51 +0900

    sound add

--- music.js
+++ music.js
@@ -1,438 +1,438 @@
 const music = [
-    {
-    title: 'I AM',
-    artist: 'IVE (아이브)'
-    },
-    {
-    title: 'UNFORGIVEN (feat. Nile Rodqers)',
-    artist: 'LE SSERAFIM (르세라핌)'
-    },
-    { 
-    title: 'Spicy',
-    artist: 'aespa'
-    },
-    {
-    title: 'Kitsch',
-    artist: 'IVE (아이브)'
-    },
-    {
-    title: '퀸카 (Queencard)',
-    artist: '(여자)아이들'
-    },
-    {
-    title: '손오공',
-    artist: '세븐틴 (SEVENTEEN)'
-    },
-    {
-    title: '꽃',
-    artist: '지수 (JISOO)'
-    },
-    {
-    title: 'Ditto',
-    artist: 'NewJeans'
-    },
-    {
-    title: '물론',
-    artist: '허각'
-    },
-    {
-    title: 'Hype boy',
-    artist: 'NewJeans'
-    },
-    {
-    title: '헤어지자 말해요',
-    artist: '박재정'
-    },
-    {
-    title: '파이팅 해야지 (Feat. 이영지)',
-    artist: '부석순 (SEVENTEEN)'
-    },
-    { 
-    title: '사랑은 늘 도망가',
-    artist: '임영웅'
-    },
-    {
-    title: 'Heaven(2023)',
-    artist: '임재현'
-    },
-    {
-    title: 'OMG',
-    artist: 'NewJeans'
-    },
-    {
-    title: 'Cupid',
-    artist: 'FIFTY FIFTY'
-    },
-    {
-    title: '우리들의 블루스',
-    artist: '임영웅'
-    },
-    {
-    title: '다시 만날 수 있을까',
-    artist: '임영웅'
-    },
-    {
-    title: 'London Boy',
-    artist: '임영웅'
-    },
-    {
-    title: 'Polaroid',
-    artist: '임영웅'
-    },
-    {
-    title: 'Teddy Bear',
-    artist: 'STAYC(스테이씨)'
-    },
-    {
-    title: '사건의 지평선',
-    artist: '윤하 (YOUNHA)'
-    },
-    { 
-    title: '이제 나만 믿어요',
-    artist: '임영웅'
-    },
-    {
-    title: '무지개',
-    artist: '임영웅'
-    },
-    {
-    title: '아버지',
-    artist: '임영웅'
-    },
-    {
-    title: 'I Don\'t Think That I Like Her',
-    artist: 'Charlie Puth'
-    },
-    {
-    title: 'Allergy',
-    artist: '(여자)아이들'
-    },
-    {
-    title: 'After LIKE',
-    artist: 'IVE (아이브)'
-    },
-    {
-    title: '안녕이라고 말하지마',
-    artist: 'V.O.S'
-    },
-    {
-    title: '인생찬가',
-    artist: '임영웅'
-    },
-    {
-    title: 'A bientot',
-    artist: '임영웅'
-    },
-    {
-    title: '사랑..그게 뭔데',
-    artist: '지아'
-    },
-    { 
-    title: 'Dangerously',
-    artist: 'Charlie Puth'
-    },
-    {
-    title: '손이 참 곱던 그대',
-    artist: '임영웅'
-    },
-    {
-    title: '사람 Pt.2 (feat. 아이유)',
-    artist: 'Agust.D'
-    },
-    {
-    title: '빛이 나는 너에게',
-    artist: '던 (DAWN)'
-    },
-    {
-    title: '사랑해 진짜',
-    artist: '임영웅'
-    },
-    {
-    title: '심(心)',
-    artist: 'DK(디셈버)'
-    },
-    {
-    title: 'FRIEND THE END',
-    artist: '볼빨간사춘기'
-    },
-    {
-    title: '연애편지',
-    artist: '임영웅'
-    },   
-    {
-    title: '나의 바람 (Wind And Wish)',
-    artist: '비투비'
-    },
-    {
-    title: 'ANTIFARGILE',
-    artist: 'LE SSERAFIM (르세라핌)'
-    },
-    { 
-    title: 'F*ck My Life',
-    artist: '세븐틴 (SEVENTEEN)'
-    },
-    {
-    title: 'Like Crazy',
-    artist: '지민'
-    },
-    {
-    title: '보금자리',
-    artist: '임영웅'
-    },
-    {
-    title: 'LOVE DIVE',
-    artist: 'IVE (아이브)'
-    },
-    {
-    title: 'CHRISTIAN',
-    artist: 'Zior Park'
-    },
-    {
-    title: 'NIGHT DANCER',
-    artist: 'imase'
-    },
-    {
-    title: 'Attention',
-    artist: 'NewJeans'
-    },
-    {
-    title: 'KNOCK',
-    artist: '이채연'
-    },
-    {
-    title: 'Dynamite',
-    artist: '방탄소년단'
-    },
-    {
-    title: 'The Planet',
-    artist: '방탄소년단'
-    },
-    { 
-    title: 'Love Me Like This',
-    artist: 'NMIXX'
-    },
-    {
-    title: 'Thirsty',
-    artist: 'aespa'
-    },
-    {
-    title: '사랑인가 봐',
-    artist: '멜로망스'
-    },
-    {
-    title: '봄날',
-    artist: '방탄소년단'
-    },
-    {
-    title: '너의 모든 순간',
-    artist: '성시경'
-    },
-    {
-    title: '사랑하기 싫어',
-    artist: '지아'
-    },
-    {
-    title: 'Butter',
-    artist: '방탄소년단'
-    },
-    {
-    title: '잠깐 시간 될까',
-    artist: '이무진'
-    },
-    {
-    title: '사랑하지 않아서 그랬어',
-    artist: '임한별'
-    },
-    {
-    title: 'Nxde',
-    artist: '(여자)아이들'
-    },
-    { 
-    title: 'Salty & Sweet',
-    artist: 'aespa'
-    },
-    {
-    title: 'Permission to Dance',
-    artist: '방탄소년단'
-    },
-    {
-    title: '건물 사이에 피어난 장미 (Rose Blossom)',
-    artist: 'H1-KEY (하이키)'
-    },
-    {
-    title: 'Angel Pt. 1 (Feat. Kodak Black, NLE Choppa, JVKE, Muni Long)',
-    artist: '지민'
-    },
-    {
-    title: 'TOMBOY',
-    artist: '(여자)아이들'
-    },
-    {
-    title: 'on the street (with J. Cole)',
-    artist: 'j-hope, J. Cole'
-    },
-    {
-    title: '부동의 첫사랑',
-    artist: '10CM'
-    },
-    {
-    title: 'Candy',
-    artist: 'NCT DREAM'
-    },
-    {
-    title: '해요 (2022)',
-    artist: '#안녕'
-    },
-    {
-    title: 'Nostalgia',
-    artist: '우디 (Woody)'
-    },
-    { 
-    title: 'STAY',
-    artist: 'The Kid LAROI, Justin Bieber'
-    },
-    {
-    title: '먼지',
-    artist: '세븐틴 (SEVENTEEN)'
-    },
-    {
-    title: 'Dreamers [Music from the FIFA World Cup Qatar 2022 Official Soundtrack] (Feat. FIFA Sound)',
-    artist: '정국, 방탄소년단'
-    },
-    {
-    title: 'That\s Hilarious',
-    artist: 'Charlie Puth'
-    },
-    {
-    title: '그라데이션',
-    artist: '10CM'
-    },
-    {
-    title: '발걸음',
-    artist: 'DK(디셈버)'
-    },
-    {
-    title: 'Monologue',
-    artist: '테이'
-    },
-    {
-    title: '취중고백',
-    artist: '김민석 (멜로망스)'
-    },   
-    {
-    title: '해금',
-    artist: 'Agust. D'
-    },
-    {
-    title: 'Say I Love You',
-    artist: '우디 (Woody)'
-    },
-    { 
-    title: 'FEARLESS',
-    artist: 'LE SSERAGIM (르세라핌)'
-    },
-    {
-    title: 'ELEVEN',
-    artist: 'IVE (아이브)'
-    },
-    {
-    title: 'Shut Down',
-    artist: 'BLACKPINK'
-    },
-    {
-    title: '그중에 그대를 만나',
-    artist: '김호중'
-    },
-    {
-    title: '한사람만 (2023) (여름날 우리 X 이홍기 (FT아일랜드))',
-    artist: '이홍기 (FT아일랜드)'
-    },
-    {
-    title: '정이라고 하자 (Feat. 10CM)',
-    artist: 'BIG Naughty (서동현)'
-    },
-    {
-    title: '내가 아니라도',
-    artist: '주호'
-    },
-    {
-    title: '밤이 무서워요 (Lonely Night)',
-    artist: '주주 시크릿'
-    },
-    {
-    title: '새삥 (Prod. ZICO) (Feat. 호미들)',
-    artist: '지코 (ZICO)'
-    },
-    {
-    title: 'Pink Venom',
-    artist: 'BLACKPINK'
-    },
-    { 
-    title: '다정히 내 이름을 부르면',
-    artist: '경서예지, 진건호'
-    },
-    {
-    title: '나의 마음에 (Seed)',
-    artist: '태양'
-    },
-    {
-    title: 'April shower',
-    artist: '세븐틴 (SEVENTEEN)'
-    },
-    {
-    title: 'VIBE (feat. Jimin of BTS)',
-    artist: '태양'
-    },
-    {
-    title: '그때 그 순간 그대로 (그그그)',
-    artist: 'WSG워너비 (가야G)'
-    },
-    {
-    title: '사랑했나봐',
-    artist: '이창섭'
-    },
-    {
-    title: '오르트구름',
-    artist: '윤하 (YOUNHA)'
-    },
-    {
-    title: '당신을 만나',
-    artist: '김효중, 송가인'
-    },  
-    ];
+  {
+    title: "I AM",
+    artist: "IVE (아이브)",
+  },
+  {
+    title: "UNFORGIVEN (feat. Nile Rodqers)",
+    artist: "LE SSERAFIM (르세라핌)",
+  },
+  {
+    title: "Spicy",
+    artist: "aespa",
+  },
+  {
+    title: "Kitsch",
+    artist: "IVE (아이브)",
+  },
+  {
+    title: "퀸카 (Queencard)",
+    artist: "(여자)아이들",
+  },
+  {
+    title: "손오공",
+    artist: "세븐틴 (SEVENTEEN)",
+  },
+  {
+    title: "꽃",
+    artist: "지수 (JISOO)",
+  },
+  {
+    title: "Ditto",
+    artist: "NewJeans",
+  },
+  {
+    title: "물론",
+    artist: "허각",
+  },
+  {
+    title: "Hype boy",
+    artist: "NewJeans",
+  },
+  {
+    title: "헤어지자 말해요",
+    artist: "박재정",
+  },
+  {
+    title: "파이팅 해야지 (Feat. 이영지)",
+    artist: "부석순 (SEVENTEEN)",
+  },
+  {
+    title: "사랑은 늘 도망가",
+    artist: "임영웅",
+  },
+  {
+    title: "Heaven(2023)",
+    artist: "임재현",
+  },
+  {
+    title: "OMG",
+    artist: "NewJeans",
+  },
+  {
+    title: "Cupid",
+    artist: "FIFTY FIFTY",
+  },
+  {
+    title: "우리들의 블루스",
+    artist: "임영웅",
+  },
+  {
+    title: "다시 만날 수 있을까",
+    artist: "임영웅",
+  },
+  {
+    title: "London Boy",
+    artist: "임영웅",
+  },
+  {
+    title: "Polaroid",
+    artist: "임영웅",
+  },
+  {
+    title: "Teddy Bear",
+    artist: "STAYC(스테이씨)",
+  },
+  {
+    title: "사건의 지평선",
+    artist: "윤하 (YOUNHA)",
+  },
+  {
+    title: "이제 나만 믿어요",
+    artist: "임영웅",
+  },
+  {
+    title: "무지개",
+    artist: "임영웅",
+  },
+  {
+    title: "아버지",
+    artist: "임영웅",
+  },
+  {
+    title: "I Don't Think That I Like Her",
+    artist: "Charlie Puth",
+  },
+  {
+    title: "Allergy",
+    artist: "(여자)아이들",
+  },
+  {
+    title: "After LIKE",
+    artist: "IVE (아이브)",
+  },
+  {
+    title: "안녕이라고 말하지마",
+    artist: "V.O.S",
+  },
+  {
+    title: "인생찬가",
+    artist: "임영웅",
+  },
+  {
+    title: "A bientot",
+    artist: "임영웅",
+  },
+  {
+    title: "사랑..그게 뭔데",
+    artist: "지아",
+  },
+  {
+    title: "Dangerously",
+    artist: "Charlie Puth",
+  },
+  {
+    title: "손이 참 곱던 그대",
+    artist: "임영웅",
+  },
+  {
+    title: "사람 Pt.2 (feat. 아이유)",
+    artist: "Agust.D",
+  },
+  {
+    title: "빛이 나는 너에게",
+    artist: "던 (DAWN)",
+  },
+  {
+    title: "사랑해 진짜",
+    artist: "임영웅",
+  },
+  {
+    title: "심(心)",
+    artist: "DK(디셈버)",
+  },
+  {
+    title: "FRIEND THE END",
+    artist: "볼빨간사춘기",
+  },
+  {
+    title: "연애편지",
+    artist: "임영웅",
+  },
+  {
+    title: "나의 바람 (Wind And Wish)",
+    artist: "비투비",
+  },
+  {
+    title: "ANTIFARGILE",
+    artist: "LE SSERAFIM (르세라핌)",
+  },
+  {
+    title: "F*ck My Life",
+    artist: "세븐틴 (SEVENTEEN)",
+  },
+  {
+    title: "Like Crazy",
+    artist: "지민",
+  },
+  {
+    title: "보금자리",
+    artist: "임영웅",
+  },
+  {
+    title: "LOVE DIVE",
+    artist: "IVE (아이브)",
+  },
+  {
+    title: "CHRISTIAN",
+    artist: "Zior Park",
+  },
+  {
+    title: "NIGHT DANCER",
+    artist: "imase",
+  },
+  {
+    title: "Attention",
+    artist: "NewJeans",
+  },
+  {
+    title: "KNOCK",
+    artist: "이채연",
+  },
+  {
+    title: "Dynamite",
+    artist: "방탄소년단",
+  },
+  {
+    title: "The Planet",
+    artist: "방탄소년단",
+  },
+  {
+    title: "Love Me Like This",
+    artist: "NMIXX",
+  },
+  {
+    title: "Thirsty",
+    artist: "aespa",
+  },
+  {
+    title: "사랑인가 봐",
+    artist: "멜로망스",
+  },
+  {
+    title: "봄날",
+    artist: "방탄소년단",
+  },
+  {
+    title: "너의 모든 순간",
+    artist: "성시경",
+  },
+  {
+    title: "사랑하기 싫어",
+    artist: "지아",
+  },
+  {
+    title: "Butter",
+    artist: "방탄소년단",
+  },
+  {
+    title: "잠깐 시간 될까",
+    artist: "이무진",
+  },
+  {
+    title: "사랑하지 않아서 그랬어",
+    artist: "임한별",
+  },
+  {
+    title: "Nxde",
+    artist: "(여자)아이들",
+  },
+  {
+    title: "Salty & Sweet",
+    artist: "aespa",
+  },
+  {
+    title: "Permission to Dance",
+    artist: "방탄소년단",
+  },
+  {
+    title: "건물 사이에 피어난 장미 (Rose Blossom)",
+    artist: "H1-KEY (하이키)",
+  },
+  {
+    title: "Angel Pt. 1 (Feat. Kodak Black, NLE Choppa, JVKE, Muni Long)",
+    artist: "지민",
+  },
+  {
+    title: "TOMBOY",
+    artist: "(여자)아이들",
+  },
+  {
+    title: "on the street (with J. Cole)",
+    artist: "j-hope, J. Cole",
+  },
+  {
+    title: "부동의 첫사랑",
+    artist: "10CM",
+  },
+  {
+    title: "Candy",
+    artist: "NCT DREAM",
+  },
+  {
+    title: "해요 (2022)",
+    artist: "#안녕",
+  },
+  {
+    title: "Nostalgia",
+    artist: "우디 (Woody)",
+  },
+  {
+    title: "STAY",
+    artist: "The Kid LAROI, Justin Bieber",
+  },
+  {
+    title: "먼지",
+    artist: "세븐틴 (SEVENTEEN)",
+  },
+  {
+    title:
+      "Dreamers [Music from the FIFA World Cup Qatar 2022 Official Soundtrack] (Feat. FIFA Sound)",
+    artist: "정국, 방탄소년단",
+  },
+  {
+    title: "Thats Hilarious",
+    artist: "Charlie Puth",
+  },
+  {
+    title: "그라데이션",
+    artist: "10CM",
+  },
+  {
+    title: "발걸음",
+    artist: "DK(디셈버)",
+  },
+  {
+    title: "Monologue",
+    artist: "테이",
+  },
+  {
+    title: "취중고백",
+    artist: "김민석 (멜로망스)",
+  },
+  {
+    title: "해금",
+    artist: "Agust. D",
+  },
+  {
+    title: "Say I Love You",
+    artist: "우디 (Woody)",
+  },
+  {
+    title: "FEARLESS",
+    artist: "LE SSERAGIM (르세라핌)",
+  },
+  {
+    title: "ELEVEN",
+    artist: "IVE (아이브)",
+  },
+  {
+    title: "Shut Down",
+    artist: "BLACKPINK",
+  },
+  {
+    title: "그중에 그대를 만나",
+    artist: "김호중",
+  },
+  {
+    title: "한사람만 (2023) (여름날 우리 X 이홍기 (FT아일랜드))",
+    artist: "이홍기 (FT아일랜드)",
+  },
+  {
+    title: "정이라고 하자 (Feat. 10CM)",
+    artist: "BIG Naughty (서동현)",
+  },
+  {
+    title: "내가 아니라도",
+    artist: "주호",
+  },
+  {
+    title: "밤이 무서워요 (Lonely Night)",
+    artist: "주주 시크릿",
+  },
+  {
+    title: "새삥 (Prod. ZICO) (Feat. 호미들)",
+    artist: "지코 (ZICO)",
+  },
+  {
+    title: "Pink Venom",
+    artist: "BLACKPINK",
+  },
+  {
+    title: "다정히 내 이름을 부르면",
+    artist: "경서예지, 진건호",
+  },
+  {
+    title: "나의 마음에 (Seed)",
+    artist: "태양",
+  },
+  {
+    title: "April shower",
+    artist: "세븐틴 (SEVENTEEN)",
+  },
+  {
+    title: "VIBE (feat. Jimin of BTS)",
+    artist: "태양",
+  },
+  {
+    title: "그때 그 순간 그대로 (그그그)",
+    artist: "WSG워너비 (가야G)",
+  },
+  {
+    title: "사랑했나봐",
+    artist: "이창섭",
+  },
+  {
+    title: "오르트구름",
+    artist: "윤하 (YOUNHA)",
+  },
+  {
+    title: "당신을 만나",
+    artist: "김효중, 송가인",
+  },
+];
 
-const info1Element = document.getElementById("info1")
+const info1Element = document.getElementById("info1");
 const titleElement = document.getElementById("title");
-const info2Element = document.getElementById("info2")
+const info2Element = document.getElementById("info2");
 const artistElement = document.getElementById("artist");
 
-var clicked = 0
+var clicked = 0;
 function showMusicInfo() {
-  const gameClearText = document.getElementById("game-clear-text")
-  const clicked_retry = document.getElementById("clear-retry-button")
-  gameClearText.classList.remove("hide")
+  const gameClearText = document.getElementById("game-clear-text");
+  const clicked_retry = document.getElementById("clear-retry-button");
+  gameClearText.classList.remove("hide");
 
   const todaysMusic = music[Math.floor(Math.random() * music.length)];
   const randomed_title = todaysMusic.title;
   const randomed_artist = todaysMusic.artist;
 
-  info1Element.textContent = "제목: "
+  info1Element.textContent = "제목: ";
   titleElement.textContent = randomed_title;
-  info2Element.textContent = "아티스트: "
+  info2Element.textContent = "아티스트: ";
   artistElement.textContent = randomed_artist;
 
-  showMeNow_show.style.display = "none"
+  showMeNow_show.style.display = "none";
 
-  clicked_retry.addEventListener("click", function(){
-    gameClearText.classList.add("hide")
-  })
-    
+  clicked_retry.addEventListener("click", function () {
+    gameClearText.classList.add("hide");
+  });
 }
 // const showInfoMusic = document.getElementById("showInfoMusic");
 // showInfoMusic.addEventListener("click", showMusicInfo,{once: true });
 
-const showInfo = document.getElementById("showInfo")
+const showInfo = document.getElementById("showInfo");
 showInfo.addEventListener("click", function () {
-  showMusicInfo()
-  showInfo.style.display = "none"
-})
+  showMusicInfo();
+  showInfo.style.display = "none";
+});
